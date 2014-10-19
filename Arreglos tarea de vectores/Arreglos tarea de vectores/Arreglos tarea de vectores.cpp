fe69e675676803e8f3b1699778cb4a76622d45ad
Nestyko <nltobon@gmail.com>
2014-10-19 15:45:01 -0430

    Agregado el case 1

--- Arreglos tarea de vectores/Arreglos tarea de vectores/Arreglos tarea de vectores.cpp
+++ Arreglos tarea de vectores/Arreglos tarea de vectores/Arreglos tarea de vectores.cpp
@@ -15,35 +15,89 @@ int _tmain(int argc, _TCHAR* argv[])
 	do
 	{
 		unsigned int seleccion;
+		cout << "1. Multiplicar 1 vector por 1 escalar" << endl;
 		cout << "2. Calcular el mayor valor de una lista de N números" << endl;
 		cout << "Seleccion: ";
 		cin >> seleccion;
 
 		switch (seleccion)
 		{
+		case 1:{
+				   //Pedir el tamaño del vector
+				   unsigned int cant;
+				   cout << endl << "Ingrese la cantidad de numeros a manejar (menor a 1000): ";
+				   cin >> cant;
+				   //Validar que no se pase de 1000
+				   while (cant >1000 || cant < 0){
+					   cout << "Cantidad invalida por favor ingrese una cantidad entre 0 y 1000";
+				   }
+				   //Declarar vector en memoria dinamica
+				   int *vector;
+				   vector = new int[CAPACITY];
+				   //Llenar el vector
+				   for (unsigned int i = 0; i <cant; i++)
+				   {
+					   cout << "Ingrese el Valor " << i + 1 << ": ";
+					   cin >> vector[i];
+				   }
+
+				   //Imprimir el vector
+				   for (unsigned int i = 0; i < cant; i++)
+				   {
+					   cout << "Vector " << i + 1 << " = \t" << vector[i];
+					   cout << endl;
+				   }
+
+				   //Pedir el escalar a multiplicar
+				   int k;
+				   cout <<endl << "Ingrese el numero escalar a multiplicar por el vector: ";
+				   cin >> k;
+
+				   for (unsigned int i = 0; i < cant; i++)
+				   {
+					   vector[i] = vector[i] * k;
+				   }
+
+				   //Imprimir el vector
+				   cout << endl << "El vector resultante es: " << endl;
+				   for (unsigned int i = 0; i < cant; i++)
+				   {
+					   cout << "Vector " << i + 1 << " = \t" << vector[i];
+					   cout << endl;
+				   }
+				   break;
+		}
+
 		case  2:{
+					//Pedir el tamaño del vector
 					unsigned int cant;
 					cout << endl <<"Ingrese la cantidad de numeros a manejar (menor a 1000): ";
 					cin >> cant;
+					//validar que no se pase de 1000
 					while (cant >1000 || cant < 0){
 						cout << "Cantidad invalida por favor ingrese una cantidad entre 0 y 1000";
 					}
+
+					//Declarar vector en memoria dinamica
 					int *vector;
 					vector = new int[CAPACITY];
 			//		int vector[CAPACITY];
 
+					//Llenar el vector
 					for (unsigned int i = 0; i <cant; i++)
 					{
 						cout << "Ingrese el Valor " << i + 1 << ": ";
 						cin >> vector[i];
 					}
 
+					//Imprimir el vector
 					for (unsigned int i = 0; i < cant; i++)
 					{
 						cout << "Vector " << i + 1 << " = \t" << vector[i];
 						cout << endl;
 					}
 
+					//Operaciones para determinar el numero mayor
 						int i1 = 0, i2 = 1;
 						do
 						{
